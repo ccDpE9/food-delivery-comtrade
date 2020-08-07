783b77bf6b792c1b62ea68af7a8fd5f2c472d3b1
ccDpE9 <z_eon00@protonmail.com>
2020-08-07 23:27:45 +0200

    Removed some domain classes, so i can get them into a shared domain class directory. Tweaked some things in FrontController, just so i get things rolling.

--- server/src/com/delivery/controller/FrontController.java
+++ server/src/com/delivery/controller/FrontController.java
@@ -8,14 +8,16 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.delivery.domain.Request;
+import com.delivery.domain.Response;
+
 public class FrontController {
-	public static int PORT = 9000;
-	private ExecutorService executors = Executors.newCachedThreadPool();
-	
 	public static void main(String[] args) {
 		try {
+			ExecutorService executors = Executors.newCachedThreadPool();
+
 			while(true) {
-				ServerSocket serverSocket = new ServerSocket(PORT);
+				ServerSocket serverSocket = new ServerSocket(8000);
 				Socket socket = serverSocket.accept();
 				Request request = parse(socket);
 				
@@ -24,10 +26,8 @@ public class FrontController {
 				    	dispatchRequest(request, socket);
 				    }
 				};
-				if(!list.contains(clijentTread)) {
-					ControlerTread.getInstanca().addClient(clijentTread);
-					executorService.execute(clijentTread);
-				}
+
+				executors.execute(clientThread);
 			}
 		} 
 		catch (IOException e) {
@@ -37,23 +37,26 @@ public class FrontController {
 	
 	private static Request parse(Socket socket) {
 		ObjectInputStream objectInputStream;
+		Request request = null;
 		
 		try {
 			objectInputStream = new ObjectInputStream(socket.getInputStream());
-			Request request = (Request) objectInputStream.readObject();
+			request = (Request) objectInputStream.readObject();
 		}
-		catch(IOException e) {
+		catch(ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		}
+		
+		return request;
 	}
 
-	private static void dispatchRequest(Request request) {
-		Dispatcher dispatcher = new Dispatcher(Request request, Socket socket);
-		Response response = dispatcher.dispatch();
-		response(response);
+	private static void dispatchRequest(Request request, Socket socket) {
+		Response response = new Response();
+		Dispatcher.dispatch(request, response);
+		respond(response, socket);
 	}
 	
-	private static void respond(Response response) {
+	private static void respond(Response response, Socket socket) {
 		try {
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 			objectOutputStream.writeObject(response);
