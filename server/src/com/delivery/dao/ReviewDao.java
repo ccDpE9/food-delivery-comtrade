1792c908a723378e3f4c662a3026f9a72d8a80e1
ccDpE9 <z_eon00@protonmail.com>
2020-09-06 19:30:43 +0200

    Review DAO that saves gives review.

---
+++ server/src/com/delivery/dao/ReviewDao.java
@@ -0,0 +1,7 @@
+package com.delivery.dao;
+
+import com.delivery.domain.Review;
+
+public interface ReviewDao {
+	public int save(Review review);
+}
---
+++ server/src/com/delivery/dao/ReviewDaoImpl.java
@@ -0,0 +1,41 @@
+package com.delivery.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.delivery.database.DBConnection;
+import com.delivery.domain.Review;
+
+public class ReviewDaoImpl implements ReviewDao {
+
+	@Override
+	public int save(Review review) {
+		int result = 0;
+		Connection conn = DBConnection.getInstance().getConnection();
+		
+		try {
+			String sql = "insert into review(user_id, restaurant_id, quality, menu_prices, services, message) values (?, ?, ?, ?, ?, ?);";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setLong(1, review.getUser().getId());
+			ps.setLong(2, review.getRestaurent().getId());
+			ps.setInt(3, review.getQuality());
+			ps.setInt(4, review.getMenuPrices());
+			ps.setInt(5, review.getServices());
+			ps.setString(6, review.getMessage());
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) result = 1;
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DBConnection.getInstance().putConnection(conn);
+		}
+		
+		return result;
+	}
+
+}
