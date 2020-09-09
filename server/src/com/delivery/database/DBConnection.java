376f869c1eafd6ceb9dcde7063b75d1961a35735
ccDpE9 <z_eon00@protonmail.com>
2020-09-09 13:51:20 +0200

    Made DB Connection Singleton, without ConnectionPooling, since it began to overly complicated transaction logic.

--- server/src/com/delivery/database/DBConnection.java
+++ server/src/com/delivery/database/DBConnection.java
@@ -5,9 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static DBConnection instance;
+	private Connection connection;
+	private volatile static DBConnection instance;
 	private static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
 	private static final String DB_URL = "jdbc:mariadb://localhost:3306/delivery_se";
+	/*
 	private static final int MAX_CON = 1;
 	private static final Connection[] buffer = new Connection[MAX_CON];
 	private int first = 0;
@@ -54,5 +56,61 @@ public class DBConnection {
 		this.free++;
 		buffer[this.last] = conn;
 		this.last = (this.last+1)%MAX_CON;
+	}
+	*/
+
+	private DBConnection() {}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public static DBConnection getInstance() {
+		if(instance == null) {
+			synchronized (DBConnection.class) {
+				if(instance == null) {
+					instance = new DBConnection();
+				}
+			}
+		}
+
+		return instance;
+	}
+
+	public void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void rollbackTransaction() {
+		try {
+			connection.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void commitTransaction() {
+		try {
+			connection.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void startTransaction() {
+		try {
+			connection = DriverManager.getConnection(DBConnection.DB_URL, "comm", "comm");
+			connection.setAutoCommit(false);
+			connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
